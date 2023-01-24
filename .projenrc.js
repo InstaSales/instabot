35bd2ff45b5fc13f462a9f12c11c11582682b6c0
Maksim Yersh <edelwud@icloud.com>
2023-01-24 16:12:35 +0300

    feat(app): add auto-approve options

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,21 @@
-const { awscdk } = require('projen');
-const project = new awscdk.AwsCdkTypeScriptApp({
-  name: 'instabot',
-  description: 'A simple instabot for sales',
+const { awscdk } = require("projen");
 
-  cdkVersion: '2.61.1',
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: "instabot",
+  description: "A simple instabot for sales",
+
+  cdkVersion: "2.61.1",
 
   release: true,
-  defaultReleaseBranch: 'main',
-  
+  defaultReleaseBranch: "main",
+
   prettier: true,
+
+  autoMerge: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["dependabot[bot]", "edelwud"],
+  },
 });
+
 project.synth();
