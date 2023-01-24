89544e44fcae29081b4bced2829c380c93497e12
Maksim Yersh <edelwud@icloud.com>
2023-01-24 16:03:50 +0300

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,11 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: 'instabot',
+  description: 'A simple instabot for sales',
+
+  cdkVersion: '2.61.1',
+
+  release: true,
+  defaultReleaseBranch: 'main',
+});
+project.synth();
